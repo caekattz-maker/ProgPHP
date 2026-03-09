a75bbc0015ea7359436d029abab41280a29b1fd5
Carlos Espinosa Kattz <caekattz@gmail.com>
2026-03-09 22:33:29 +0000

    CREATED estructura base

---
+++ src/app/Http/Controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    // Normalmente está vacío o tiene muy poco código
+}
---
+++ src/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+# use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+
+#Route::get('/user', function (Request $request) {
+#    return $request->user();
+#})->middleware('auth:api');
+
+#Route::apiResource('eventos', EventoController::class);
+
+Route::get('/eventos', [EventoController::class, 'index']);
+Route::post('/eventos', [EventoController::class, 'store']);
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+Route::put('/eventos/{id}', [EventoController::class, 'update']);
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
