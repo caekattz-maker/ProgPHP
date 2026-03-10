ea84dfced818227f6b8f6a04e54d116792b25e26
Carlos Espinosa Kattz <caekattz@gmail.com>
2026-03-10 02:25:36 +0000

    Agregadas entidades Ponente y Asistente a la API

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,9 @@
 # use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventoController;
+use App\Http\Controllers\PonenteController;
+use App\Http\Controllers\AsistenteController;
+
 
 #Route::get('/user', function (Request $request) {
 #    return $request->user();
@@ -15,4 +18,8 @@ Route::post('/eventos', [EventoController::class, 'store']);
 Route::get('/eventos/{id}', [EventoController::class, 'show']);
 Route::put('/eventos/{id}', [EventoController::class, 'update']);
 Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+Route::apiResource('ponentes', PonenteController::class);
+Route::apiResource('asistentes', AsistenteController::class);
+
 
